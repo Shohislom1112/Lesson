048167580721caeda3ab0ca8a035c025354ab0c0
Shohislom1112 <ravshanovshohislom11@gmail.com>
2024-03-12 19:08:54 +0500

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,14 @@
 // import {   } from "@/assets/images/images/ .jpg";
 
-// import {   } from "@/assets/images";
-import { getData } from "@/service/get-data";
-import { Card } from "@/components/card";
+import {   } from "@/assets/images";
 
 import React from "react";
 
-export default async function Home() {
-  const data = await getData();
+const Home = () => {
   return (
     // <div>
      <div className="flex flex-wrap gap-16 justify-center border-spacing-1 mt-60">
        
-          {/* <div
-           
-            className=" hover:scale-100 transition duration-500 cursor-pointer object-cover hover:shadow-2xl "
-            style={{ width: "18em", height: "22em" }}
-          >
-            <img
-              className="transition duration-300 ease-in-out w-72 h-44 "
-              src={`https://www.androidauthority.com/xiaomi-14-ultra-release-date-price-specs-3357551/`}
-              alt="Xiomi14"
-            />
-            <div className="texts p-5">
-              <p className="font-bold text-xl"></p>
-              <p className="pt-5 flex"> 
-                <p className="font-semibold pr-1">Name: XIAOMI 14 12/512 Black</p>
-               
-              </p>
-              <p className="flex">
-                <p className="font-semibold pr-1">Storage: RAM:16GB, Built-in:1TR</p>
-              </p>
-              <p className="flex">
-                <p className="font-semibold pr-1">Protsessor: Snapdragon 8 Gen 3</p>
-              </p>
-              <p className="flex">
-                <p className="font-semibold pr-1">Cost: 1400 $</p>
-              </p>
-            </div>
-          </div>
           <div
            
             className=" hover:scale-100 transition duration-500 cursor-pointer object-cover hover:shadow-2xl "
@@ -281,14 +251,38 @@ export default async function Home() {
                 <p className="font-semibold pr-1">Cost: 1400 $</p>
               </p>
             </div>
-          </div> */}
-          {data?.map((item) => (
-        <Card key={item.id} {...item} />
-      ))}
+          </div>
+          <div
+           
+            className=" hover:scale-100 transition duration-500 cursor-pointer object-cover hover:shadow-2xl "
+            style={{ width: "18em", height: "22em" }}
+          >
+            <img
+              className="transition duration-300 ease-in-out w-72 h-44 "
+              src={`https://www.androidauthority.com/xiaomi-14-ultra-release-date-price-specs-3357551/`}
+              alt="Xiomi14"
+            />
+            <div className="texts p-5">
+              <p className="font-bold text-xl"></p>
+              <p className="pt-5 flex"> 
+                <p className="font-semibold pr-1">Name: XIAOMI 14 12/512 Black</p>
+               
+              </p>
+              <p className="flex">
+                <p className="font-semibold pr-1">Storage: RAM:16GB, Built-in:1TR</p>
+              </p>
+              <p className="flex">
+                <p className="font-semibold pr-1">Protsessor: Snapdragon 8 Gen 3</p>
+              </p>
+              <p className="flex">
+                <p className="font-semibold pr-1">Cost: 1400 $</p>
+              </p>
+            </div>
+          </div>
         
       </div>
     // </div>
   );
 };
 
-// export default Home;
+export default Home;
--- src/components/store-data.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const StoreData: React.FC = () => {
-  return <div>StoreData</div>;
-};
